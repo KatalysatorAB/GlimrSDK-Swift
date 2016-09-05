e0bb897b50cc1452c942fb618f667f325c63e674
Sven Roeder <svenroed@gmail.com>
2016-09-05 11:15:08 +0200

    Initial Commit

---
+++ SampleSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleSwift
+//
+//  Created by Sven Röder on 2016-09-05.
+//  Copyright © 2016 Glimr AB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
